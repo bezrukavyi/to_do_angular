06a16446b2156a8878a16227a3a8ec9db0324c6e
Yaroslav Bezrukavyi <yaroslav555@gmail.com>
2017-03-04 18:46:22 +0200

    Production setting

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module ToDoAngular
       g.test_framework :rspec
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
+
+    config.assets.initialize_on_precompile = false
   end
 end
